4b16459db59ca0652344f5ee697aae0bb5bf4070
bello <bellosoliu12@gmail.com>
2022-11-24 09:58:44 +0100

    First push

---
+++ CustomNullException/NullException.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CustomNullException
+{
+    public class CustomNullException : Exception
+    {
+        public CustomNullException()
+        {
+        }
+        public CustomNullException(string message) : base(message)
+        {
+        }
+        public CustomNullException(string message, Exception innerexception) : base(message, innerexception)
+        {
+        }
+    }
+}
---
+++ CustomNullException/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomNullException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string or empty to test for Exception");
+            string? text = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new CustomNullException("This is a Null Exception");
+            }
+            else
+            {
+                Console.WriteLine("You entered: {0}, a Custom Exception." ,text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
